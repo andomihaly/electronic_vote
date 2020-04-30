7665235289a94e7bee49d4669cbd72ce7b3c995e
Zsolt Füzesi <zsolt.fuzesi@idomsoft.hu>
2020-04-30 22:11:23 +0200

    add referendum voting result view step definitions

---
+++ electronic-vote/src/test/java/hu/am/ongoingvotingresultview/systemtest/ReferendumVotingResultViewStepDef.java
@@ -0,0 +1,88 @@
+package hu.am.ongoingvotingresultview.systemtest;
+
+import io.cucumber.java.hu.Akkor;
+import io.cucumber.java.hu.Amennyiben;
+import io.cucumber.java.hu.Amikor;
+
+public class ReferendumVotingResultViewStepDef {
+
+    @Amennyiben("elindult a népszavazás")
+    public void referendumStarted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Amennyiben("még nem zárult le")
+    public void referendumNotClosedYet() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Amikor("megnézem a választási adatokat")
+    public void iSeeReferendumResult() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Akkor("látom az aktuális összegzett országos részvételi arányt")
+    public void iSeeTheActualCumulativeVoterTurnoutResultByCountry() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Akkor("nem látom az aktuális szavazási eredményeket")
+    public void iCantSeeTheActualResult() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Amikor("kiválasztom a megyét vagy a fővárost")
+    public void iChooseTheCountyOrTheCapital() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Akkor("látom az aktuális összegzett részvételi arányt")
+    public void iSeeTheActualCumulativeVoterTurnoutResult() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Amikor("kiválasztok egy települést")
+    public void iChooseASettlement() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Akkor("látom az aktuális összegzett települési részvételi arányt")
+    public void iSeeTheActualCumulativeVoterTurnoutResultBySettlement() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Amikor("kiválasztok egy kerületet")
+    public void iChooseADistrict() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Akkor("látom az aktuális összegzett kerületi részvételi arányt")
+    public void iSeeTheActualCumulativeVoterTurnoutResultByDistrict() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @Amikor("kiválasztok egy körzetet")
+    public void iChooseAnArea() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Akkor("látom az aktuális körzeti részvételi arányt")
+    public void iSeeTheActualCumulativeVoterTurnoutResultByArea() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
